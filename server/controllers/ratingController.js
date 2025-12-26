e26173657f06e723328c5c6e31facb763362c0a7
imrankhan919 <imrankhan919.ik@gmail.com>
2025-12-26 14:51:32 +0530

    server side completed

--- server/controllers/ratingController.js
+++ server/controllers/ratingController.js
@@ -1,9 +1,47 @@
+import Rating from "../models/ratingModel.js"
+
+
+
 const getRatings = async (req, res) => {
-    res.send("All Ratings...")
+
+    let freelancerId = req.params.fid
+
+    let ratings = await Rating.find({ freelancer: freelancerId }).populate('user')
+
+    if (!ratings) {
+        res.status(404)
+        throw new Error("Ratings Not Found!")
+    }
+
+    res.status(200).json(ratings)
 }
 
 const addRating = async (req, res) => {
-    res.send("Rating Added..")
+
+    let userId = req.user._id
+
+    const { rating, review } = req.body
+
+    if (!rating || !review) {
+        res.status(409)
+        throw new Error("Please Fill All Details!")
+    }
+
+
+
+    const newRating = await Rating.create({
+        user: userId,
+        rating,
+        review
+    })
+
+
+    if (!newRating) {
+        res.status(409)
+        throw new Error("Rating Not Created!")
+    }
+
+    res.status(201).json(newRating)
 }
 
 
--- server/routes/ratingRoutes.js
+++ server/routes/ratingRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
 
 import ratingController from "../controllers/ratingController.js"
+import protect from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 
 router.get("/", ratingController.getRatings)
-router.post("/", ratingController.addRating)
+router.post("/", protect.forAuthUsers, ratingController.addRating)
 
 
 export default router
